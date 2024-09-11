4071cd3a06c17c8d981f22017815363943965870
Malatik16 <kustarnikov03@bk.ru>
2024-09-11 11:17:46 +0500

    PYTHON

---
+++ test/test.py
@@ -0,0 +1,17 @@
+a = 1
+b = 2
+if (a > 0 and b >0):
+   print("1")
+   print("2")
+   print("3")
+   print(a,b)
+else:
+   print("Ну бывает")
+   print("лол")
+print("лол-кек")
+
+#text = int(input("Введите текст -> "))
+#print(text)
+
+for i in range(5):
+   print(i)
